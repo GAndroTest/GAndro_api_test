ec63e36f22d8b7b1f3124288057e7885d08c140d
juanlu <jlljunior_90@hotmail.com>
2018-09-29 12:04:50 +0200

    Api capabilities
    Added Basic controller with api capabilities

--- src/main/java/es/us/gandro/api/GAndroApiApplication.java
+++ src/main/java/es/us/gandro/api/GAndroApiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class GAndroApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GAndroApiApplication.class, args);
+    }
 }
